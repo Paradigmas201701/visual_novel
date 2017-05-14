9e040c9ec9639b444f90cbfbf12e58638226e09b
Lucas Rufino <lucaslrt@outlook.com>
2017-05-14 02:41:59 -0300

    Fixing conteudo_da_fala.

--- novel.pl
+++ novel.pl
@@ -13,24 +13,27 @@ emite_fala(Personagem, Alternativa, NumeroFala) :-
 
 ler_txt(File, Personagem, Alternativa, NumeroFala) :-
     open(Arquivo, read, Conteudo),
-    get_char(Conteudo, LetraAtual),
-    conteudo_da_fala(LetraAtual, Personagem),
+    get_char(Conteudo, LetraPersonagem),
+    conteudo_da_fala(LetraPersonagem, Personagem),
     close(Conteudo).
 
 % !. interrompe a leitura do arquivo.
 conteudo_da_fala(end_of_file, _) :- !.
 
 % se encontrar falas do personagem específico, continua a leitura.
-conteudo_da_fala(Personagem, _) :-
-    get_char(Conteudo, Alternativa).
+conteudo_da_fala(Personagem, Conteudo) :-
+    get_char(Conteudo, LetraAlternativa),
+    conteudo_da_fala(LetraAlternativa, Alternativa).
 
 % se encontrar a alternativa da fala específica, continua a leitura.
-conteudo_da_fala(Alternativa, _) :-
-    conteudo_da_fala(Conteudo, NumeroFala).
+conteudo_da_fala(Alternativa, Conteudo) :-
+    get_char(Conteudo, LetraNumero),
+    conteudo_da_fala(LetraNumero, NumeroFala).
 
-% se encontrar o número da fala específica, continua a leitura.
-conteudo_da_fala(NumeroFala, _) :-
-    conteudo_da_fala(Conteudo, _).
+% se encontrar o número da fala específica, começa a leitura da fala.
+conteudo_da_fala(NumeroFala, Conteudo) :-
+    get_char(Conteudo, LetraFala)
+    conteudo_da_fala(LetraFala, Conteudo).
 
 % ';' representa o fim de fala do personagem.
 conteudo_da_fala(';', _) :- !.
@@ -38,5 +41,5 @@ conteudo_da_fala(';', _) :- !.
 % printa a letra e continua a leitura.
 conteudo_da_fala(Letra, Conteudo) :-
     write(Letra),
-    get_char(Conteudo, LetraPegada),
-    conteudo_da_fala(LetraPegada, Conteudo).
+    get_char(Conteudo, ProxLetra),
+    conteudo_da_fala(ProxLetra, Conteudo).
